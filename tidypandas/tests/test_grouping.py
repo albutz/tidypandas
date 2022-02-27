bc7ae75a2fa3025de838125e55730024ae6c5e41
Alexander Butz <alex.butz@gmail.com>
2022-02-27 19:18:06 +0100

    test(grouping.py): Add test when multiple columns to group by are not found

--- tidypandas/tests/test_grouping.py
+++ tidypandas/tests/test_grouping.py
@@ -42,3 +42,12 @@ def test_add_count_missing_single_col(example_df: pd.DataFrame) -> None:
     with pytest.raises(GroupsNotFoundError) as excinfo:
         add_count(example_df, ["some_missing_col"])
     assert "Column some_missing_col is not included in the DataFrame." in str(excinfo.value)
+
+
+def test_add_count_missing_multiple_cols(example_df: pd.DataFrame) -> None:
+    with pytest.raises(GroupsNotFoundError) as excinfo:
+        add_count(example_df, ["some_missing_col", "some_other_missing_col"])
+    assert (
+        "Columns some_missing_col and some_other_missing_col are not included in the DataFrame."
+        in str(excinfo.value)
+    )
